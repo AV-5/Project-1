fa3a63e5a60bb4907b5985a95534a61822858402
AV <mvav1998@gmail.com>
2025-10-23 19:20:53 +0530

    post(Mongodb_test)

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// CORS config
+app.use(cors({
+  origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Import routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import authRouter from "./routes/auth.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
+
+app.get('/instagram', (req, res) => {
+  res.send('Welcome to instagram');
+});
+
+export default app;
---
+++ src/db/database.js
@@ -0,0 +1,14 @@
+// src/db/database.js
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log(" MongoDB connected");
+  } catch (error) {
+    console.error(" MongoDB connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
